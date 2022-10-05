67a69b6825cb3a61dc60522df5a13f283bdbc811
Rafael Guimarães <rafael.guimaraes@usabit.com.br>
2022-10-05 17:27:37 -0300

    salvando trabalho inicial

---
+++ ola mundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
